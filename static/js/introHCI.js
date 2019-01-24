12317d2d8a137f789ec2d31ed1ecae5c5d30362a
Jennifer Sandoval <jennifersandoval94@gmail.com>
2019-01-24 10:39:53 -0800

    modified js file to add JQuery

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -12,7 +12,29 @@ function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
-
+	$("a.thumbnail").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("#testjs").click(buttonClick);
+	
+}
+
+function buttonClick(e){
+	$("#testjs").text("Oh heck!");
+	$(".jumbotron p").addClass("active");
+}
+
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event    
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+    	description.fadeToggle();
+    }
 }
